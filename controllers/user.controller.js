a95c5778fa2ec0d5215b6d2f9bd9ebd1102a4b7b
someshwarholkar <someshwarsholkar2004@gmail.com>
2024-06-27 09:45:47 +0530

    server setup

---
+++ controllers/user.controller.js
@@ -0,0 +1,28 @@
+const asyncHandler = require("express-async-handler")
+const Order = require("../model/Order")
+
+exports.userGetAllOrders = asyncHandler(async (req, res) => {
+    const result = await Order.find({ user: req.params.id })
+    res.json({ message: "Users Fetch Sucess" })
+})
+exports.userGetOrderDetails = asyncHandler(async (req, res) => {
+    const result = await Order.findbyId(req.params.id)
+    res.json({ message: "User GetOrderDetails Sucess" })
+})
+exports.userUpdatePassword = asyncHandler(async (req, res) => {
+    res.json({ message: "User UpdatePassword Sucess" })
+})
+exports.userPlacedOrder = asyncHandler(async (req, res) => {
+    await Order.create(req.body)
+    res.json({ message: "User PlacedOrder Sucess" })
+})
+exports.userCancelOrder = asyncHandler(async (req, res) => {
+    await Order.findByIdAndUpdate(req.params.id, { status: "calcel" })
+    res.json({ message: "User CancelOrder Sucess" })
+})
+
+// userGetAllOrders
+// userGetOrderDetails
+// userUpdatePassword
+// userPlacedOrder
+// userCancelOrder
