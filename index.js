54b5e41bd8b817db33f8f63dea7dd17fd6de6724
someshwarholkar <someshwarsholkar2004@gmail.com>
2024-07-04 13:50:10 +0530

    dist add i server

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(cors({
     credentials: true
 }))
 app.use(express.json())  // req.body madhe data takayche kam
+app.use(express.static("public"))
 
 app.use("/api/admin", require("./routes/admin.routes"))
 app.use("/api/auth", require("./routes/auth.routes"))
