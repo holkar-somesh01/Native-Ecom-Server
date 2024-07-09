a957f7f0154a852d446c5dc4fc06af3c265ca85f
someshwarholkar <someshwarsholkar2004@gmail.com>
2024-07-09 12:33:52 +0530

    apk upload

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static("public"))
 app.use("/api/admin", require("./routes/admin.routes"))
 app.use("/api/auth", require("./routes/auth.routes"))
 app.use("/api/user", require("./routes/user.routes"))
-app.use("/api/public", require("./routes//public.routes"))
+app.use("/api/public", require("./routes/public.routes"))
 
 app.use("*", (req, res) => {
     res.status(404).json({ message: "Resours Not found" })
